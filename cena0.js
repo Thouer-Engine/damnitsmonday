7c3a41e942b757de84f1d33d1def10bf4a89dac1
thonySQ sq <anthony.sq@aluno.ifsc.edu.br>
2023-08-21 20:17:47 +0000

    add uma cena e uma imagem

---
+++ cena0.js
@@ -0,0 +1,16 @@
+export default class cena0 extends Phaser.Scene {
+    constructor () {
+      super('cena0')
+    }
+
+    preload () { 
+        this.load.image('ifsc-sj-2014', './assets/ifsc-sj-2014.png')
+    }
+    
+    create () { 
+        this.imagem = this.add.image(400, 225, 'ifsc-sj-2014')
+    }
+    
+    update () { }
+}
+
--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 /*importação dos objeto*/
 
+import cena0 from './cena0.js'
 import config from './config.js'
 
 class Game extends Phaser.Game {
     constructor () {
         super(config)
-}
+
+        this.scene.add('cena0',cena0)
+        this.scene.start('cena0')
+    }
 }
 
 window.onload = () => {
