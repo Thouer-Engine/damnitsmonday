8b908da1aad0884fee011bfb4d02ea81dd583330
thonySQ sq <anthony.sq@aluno.ifsc.edu.br>
2023-08-25 19:30:29 +0000

    personagem

--- cliente/js/cena0.js
+++ cliente/js/cena0.js
@@ -4,11 +4,40 @@ export default class cena0 extends Phaser.Scene {
     }
 
     preload() {
+
         this.load.image('ifsc-sj-2014', '../assets/fundo_final.png')
+        this.load.spritesheet('derek', '../assets/derek.png',
+            {
+                frameWidth: 64,
+                frameHeight: 64
+            })
+
     }
 
     create() {
-        this.imagem = this.add.image(400, 225, 'ifsc-sj-2014')
+        this.add.image(400, 225, 'ifsc-sj-2014')
+        this.personagem = this.physics.add.sprite(400, 225, 'derek')
+            .setInteractive()
+            .on('pointerdown', () => {
+                this.personagem.anims.play('derek-direita')
+                this.personagem.setVelocityX(100)
+            })
+            
+            
+            /*.on('pointerup', () => {})
+            .on('pointerup', () => {})*/
+        
+        this.anims.create({
+            key: 'derek-direita',
+            frames: this.anims.generateFrameNumbers('derek',{
+            start: 8,
+            end: 11    
+            }),
+            frameRate: 6,
+            repeat: -1
+    
+        })
+        
     }
 
     update() { }
