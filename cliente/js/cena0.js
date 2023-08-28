885c6f37cc65be2f8d9997a02eaea092715de2d3
thonySQ sq <anthony.sq@aluno.ifsc.edu.br>
2023-08-28 20:27:57 +0000

    teste animação botoẽs

--- cliente/js/cena0.js
+++ cliente/js/cena0.js
@@ -18,10 +18,27 @@ export default class cena0 extends Phaser.Scene {
                 frameWidth: 60,
                 frameHeight:90
             })
+        
         this.load.spritesheet('direita', '../assets/direita.png', {
             frameWidth: 64, 
             frameHeight: 64
         })
+        this.load.spritesheet('esquerda', '../assets/esquerda.png', {
+            frameWidth: 64,
+            frameHeight: 64
+        })
+        this.load.spritesheet('cima', '../assets/cima.png', {
+            frameWidth: 64,
+            frameHeight: 64
+        })
+        this.load.spritesheet('baixo', '../assets/baixo.png', {
+            frameWidth: 64,
+            frameHeight: 64
+        })
+
+
+
+
    
     }
 
@@ -41,33 +58,32 @@ export default class cena0 extends Phaser.Scene {
         this.physics.add.collider(this.beto, chao)
         this.physics.add.collider(this.plinio,chao)
             
-            /*.on('pointerup', () => {})
-            .on('pointerup', () => {})*/
-        
-        this.anims.create({
-            key: 'beto-direita',
-            frames: this.anims.generateFrameNumbers('beto',{
-            start: 0,
-            end: 1    
-            }),
-            frameRate: 8,
-            repeat: -1
-        })
+
         this.anims.create({
             key: 'plinio-direita',
             frames: this.anims.generateFrameNumbers('plinio', {
                 start: 0,
                 end: 8
             }),
-            frameRate: 6,
+            frameRate: 8,
             repeat: -1
         })
 
         this.anims.create({
             key: 'plinio-parado',
             frames: this.anims.generateFrameNumbers('plinio', {
-                start: 19,
+                start: 18,
                 end: 24
+            }),
+            frameRate: 2,
+            repeat: -1
+        })
+
+        this.anims.create({
+            key: 'plinio-esquerda',
+            frames: this.anims.generateFrameNumbers('plinio', {
+                start: 9,
+                end: 17
             }),
             frameRate: 2,
             repeat: -1
@@ -85,7 +101,15 @@ export default class cena0 extends Phaser.Scene {
                     this.plinio.setVelocityX(0)
                     this.plinio.anims.play('plinio-parado', true)
                 })
+                
         
+        this.esquerda = this.add.sprite(80, 350, 'esquerda')
+            .setInteractive()
+            .on('pointerdown', () => {
+                this.plinio.setVelocityX(-100)
+                this.esquerda.setFrame(1)
+                this.plinio.anims.play('plinio-esquerda', true)
+                })
         /*fazer o mesmo para o beto*/
         
     }
